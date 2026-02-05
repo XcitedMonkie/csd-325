46b8fb5dadbabe32beaa10df34b4c5b69c672007
dfryer@my365.bellevue.edu <dfryer@my365.bellevue.edu>
2026-02-05 14:37:48 -0600

    Added notes to the sitka program

--- module - 4/sitka_high_low_DBF.py
+++ module - 4/sitka_high_low_DBF.py
@@ -5,6 +5,7 @@ from matplotlib import pyplot as plt
 
 import os
 
+# Create a function to get the data from the CSV file
 def GetWeatherData():
     
     script_dir = os.path.dirname(__file__)
@@ -17,10 +18,10 @@ def GetWeatherData():
         reader = csv.reader(f)
         header_row = next(reader)
 
-        # Get dates and high temperatures from this file.
+        # Get dates and high temperatures from this file. 
         dates, highs, lows= [], [], []
         for row in reader:
-            current_date = datetime.strptime(row[2], '%m/%d/%Y') #datetime.strptime(row[2], '%Y-%m-%d')
+            current_date = datetime.strptime(row[2], '%m/%d/%Y') #datetime.strptime(row[2], '%Y-%m-%d') Had to change this line as the datetime was not in the same format defaulted here
             dates.append(current_date)
             high = int(row[5])
             highs.append(high)
@@ -29,6 +30,7 @@ def GetWeatherData():
             
     return dates, highs, lows
 
+# create a function to display the display get the dates, temps, line color, and title from the main function
 def PlotAndDisplay(dates, temps, color, title):
     # Plot the high temperatures.
     #plt.style.use('seaborn')
@@ -44,9 +46,12 @@ def PlotAndDisplay(dates, temps, color, title):
 
     plt.show()
     
+# Start of the program here.
 def Main():
+    # start by getting the dates,highs, and lows from the function 
     dates, highs, lows = GetWeatherData()
     
+    # create a "Menu" and keep it looping until the user exits with a number 3
     while True:
         print("Welcome to the Sitka Weather Program")
         print("Please make a selection from below")
@@ -58,6 +63,7 @@ def Main():
         
         userSelect = input("Enter your choice (1,2,3) ")
         
+        # determin what the user selected. If its a 1 or 2 call the plot and display function if its a 3 exit the program any other option start the loop over.
         if userSelect == "1":
             PlotAndDisplay(dates, highs, color = 'red', title= "Daily High Temps - 2018")
         elif userSelect == "2":
